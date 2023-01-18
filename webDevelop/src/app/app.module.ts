d467a694eb6458f17fada69482fa3c67897ed541
StefanKinto <39180652+stefankino@users.noreply.github.com>
2023-01-17 20:06:05 -0400

    add nav

--- webDevelop/src/app/app.module.ts
+++ webDevelop/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainPageComponent } from './main-page/main-page.component';
+import { NavComponent } from './nav/nav.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MainPageComponent
+    MainPageComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
